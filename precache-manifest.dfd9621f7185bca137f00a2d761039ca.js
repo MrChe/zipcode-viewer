8b140eb21dfdc8317a976a4fdf8917a7b0ca96fe
LeonidKuznetsov <appleboyleonid@gmail.com>
2019-01-12 17:45:48 +0200

    Updates

---
+++ precache-manifest.dfd9621f7185bca137f00a2d761039ca.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a80095d555e82ac9b31b",
+    "url": "/zipcode-viewer/static/js/runtime~main.a80095d5.js"
+  },
+  {
+    "revision": "d65595269c243450560e",
+    "url": "/zipcode-viewer/static/js/main.d6559526.chunk.js"
+  },
+  {
+    "revision": "31c5046a0480c945bb33",
+    "url": "/zipcode-viewer/static/js/1.31c5046a.chunk.js"
+  },
+  {
+    "revision": "c0589ea2d0c201bbdf1ae5d2169cfe01",
+    "url": "/zipcode-viewer/index.html"
+  }
+];
